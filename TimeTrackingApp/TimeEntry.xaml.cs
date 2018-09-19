d08e03fcd88bcdd5bf821adeaa504a22271dcbbd
Desislava <desigeorgieva82@gmail.com>
2018-09-19 11:32:56 +0300

    time entry fix

---
+++ TimeTrackingApp/TimeEntry.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace TimeTrackingApp
+{
+    /// <summary>
+    /// Interaction logic for TimeEntry.xaml
+    /// </summary>
+    public partial class TimeEntry : Window
+    {
+        public TimeEntry()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+
+            TimeTrackingApp.TimeTrackingDataSet timeTrackingDataSet = ((TimeTrackingApp.TimeTrackingDataSet)(this.FindResource("timeTrackingDataSet")));
+            // Load data into the table tbl_TimeEntry. You can modify this code as needed.
+            TimeTrackingApp.TimeTrackingDataSetTableAdapters.tbl_TimeEntryTableAdapter timeTrackingDataSettbl_TimeEntryTableAdapter = new TimeTrackingApp.TimeTrackingDataSetTableAdapters.tbl_TimeEntryTableAdapter();
+            timeTrackingDataSettbl_TimeEntryTableAdapter.Fill(timeTrackingDataSet.tbl_TimeEntry);
+            System.Windows.Data.CollectionViewSource tbl_TimeEntryViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("tbl_TimeEntryViewSource")));
+            tbl_TimeEntryViewSource.View.MoveCurrentToFirst();
+        }
+    }
+}
--- TimeTrackingApp/UserRegistration.xaml.cs
+++ TimeTrackingApp/UserRegistration.xaml.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Data.Entity;
 
 namespace TimeTrackingApp
 {
@@ -19,24 +9,39 @@ namespace TimeTrackingApp
     /// </summary>
     public partial class UserRegistration : Window
     {
+        TimeTrackingEntities context = new TimeTrackingEntities();
+        CollectionViewSource tbl_UsersViewSource;
+        CollectionViewSource credentialsViewSource;     
+
         public UserRegistration()
         {
             InitializeComponent();
+            tbl_UsersViewSource = ((CollectionViewSource)(FindResource("tbl_UsersViewSource")));
+            credentialsViewSource = ((CollectionViewSource)(FindResource("tbl_CredentialsViewSource")));
+            DataContext = this;
+
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
 
-            TimeTrackingApp.TimeTrackingDataSet timeTrackingDataSet = ((TimeTrackingApp.TimeTrackingDataSet)(this.FindResource("timeTrackingDataSet")));
-            // Load data into the table tbl_Users. You can modify this code as needed.
-            TimeTrackingApp.TimeTrackingDataSetTableAdapters.tbl_UsersTableAdapter timeTrackingDataSettbl_UsersTableAdapter = new TimeTrackingApp.TimeTrackingDataSetTableAdapters.tbl_UsersTableAdapter();
-            System.Windows.Data.CollectionViewSource tbl_UsersViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("tbl_UsersViewSource")));
-            tbl_UsersViewSource.View.MoveCurrentToFirst();
-            // Load data into the table tbl_Credentials. You can modify this code as needed.
-            TimeTrackingApp.TimeTrackingDataSetTableAdapters.tbl_CredentialsTableAdapter timeTrackingDataSettbl_CredentialsTableAdapter = new TimeTrackingApp.TimeTrackingDataSetTableAdapters.tbl_CredentialsTableAdapter();
-            timeTrackingDataSettbl_CredentialsTableAdapter.Fill(timeTrackingDataSet.tbl_Credentials);
-            System.Windows.Data.CollectionViewSource tbl_CredentialsViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("tbl_CredentialsViewSource")));
-            tbl_CredentialsViewSource.View.MoveCurrentToFirst();
+            //TimeTrackingApp.TimeTrackingDataSet timeTrackingDataSet = ((TimeTrackingApp.TimeTrackingDataSet)(this.FindResource("timeTrackingDataSet")));
+            //// Load data into the table tbl_Users. You can modify this code as needed.
+            //TimeTrackingApp.TimeTrackingDataSetTableAdapters.tbl_UsersTableAdapter timeTrackingDataSettbl_UsersTableAdapter = new TimeTrackingApp.TimeTrackingDataSetTableAdapters.tbl_UsersTableAdapter();
+            //System.Windows.Data.CollectionViewSource tbl_UsersViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("tbl_UsersViewSource")));
+            //tbl_UsersViewSource.View.MoveCurrentToFirst();
+            //// Load data into the table tbl_Credentials. You can modify this code as needed.
+            //TimeTrackingApp.TimeTrackingDataSetTableAdapters.tbl_CredentialsTableAdapter timeTrackingDataSettbl_CredentialsTableAdapter = new TimeTrackingApp.TimeTrackingDataSetTableAdapters.tbl_CredentialsTableAdapter();
+            //timeTrackingDataSettbl_CredentialsTableAdapter.Fill(timeTrackingDataSet.tbl_Credentials);
+            //System.Windows.Data.CollectionViewSource tbl_CredentialsViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("tbl_CredentialsViewSource")));
+            //tbl_CredentialsViewSource.View.MoveCurrentToFirst();
+
+            context.tbl_Users.Load();
+            tbl_UsersViewSource.Source = context.tbl_Users.Local;
+           // context.tbl_Credentials.Load();
+            
+           // credentialsViewSource.Source = context.tbl_Credentials.Local;
+           
         }
     }
 }
